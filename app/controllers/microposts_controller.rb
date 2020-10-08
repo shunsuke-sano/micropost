04a94c6a0f6e200550d756d54598780707d5cf95
shunsuke.sano <avoshunsuke@gmail.com>
2020-10-08 04:52:26 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,7 +8,7 @@ class MicropostsController < ApplicationController
       flash[:success] = 'メッセージを投稿しました。'
       redirect_to root_url
     else
-      @microposts = current_user.microposts_order(id: :desc).page(params[:page])
+      @microposts = current_user.feed_microposts_order(id: :desc).page(params[:page])
       flash[:danger] = 'メッセージの投稿に失敗しました。'
       render 'toppage/index'
     end
